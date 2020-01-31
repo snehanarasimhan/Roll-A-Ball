3edbd98985d170246b89ce7fa00d1beb843c82f6
snehanarasimhan <snehanarasimhan@ymail.com>
2020-01-30 23:55:54 -0700

    Assests

---
+++ Assets/Control_Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_Player : MonoBehaviour
+{
+    public float velocity;
+
+    private Rigidbody rigibody;
+    private int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigibody = GetComponent<Rigidbody>();
+        counter = 0;
+    }
+
+    void fixed_update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontal, 0.0f, vertical);
+
+        rigibody.AddForce(move * velocity);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            counter = counter + 1;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocity;
+    public Text counter;
+
+    private Rigidbody rigibody;
+    private int count;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigibody = GetComponent<Rigidbody>();
+        count = 0;
+        counter.text = "Count of user: " + count.ToString();
+    }
+
+
+    void fixed_update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontal, 0.0f, vertical);
+
+        rigibody.AddForce(move * velocity);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            counter.text = "Count of user: " + count.ToString(); 
+        }
+    }
+}
